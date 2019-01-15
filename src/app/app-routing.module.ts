9b1ea755f15f66c82f5180e76aceb19e39fceb29
Daniel Smith <danielsmith.2796@googlemail.com>
2019-01-15 10:17:41 +0000

    router now working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { MinesweeperComponent } from './minesweeper/minesweeper.component';
+import { SnakeComponent } from './snake/snake.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'minesweeper', component: MinesweeperComponent},
+  {path: 'snake', component: SnakeComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  declarations: [],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
